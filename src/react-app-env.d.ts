c9e17096bd58a39f7cec5b62c867ceda1b2f5e68
David Rodriguez <davrdz25@yahoo.com.mx>
2025-05-09 15:24:46 -0600

    Remove unused  IPC's and add test connection

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,7 +4,7 @@ interface Window {
     electron: {
         connectDatabase: () => Promise<void>;
         existsConfigFile: () => Promise<void>;
-        setDatabaseConfig: (config: any) => Promise<void>;
         createConfigFile:(config: any) => Promise<void>;
+        testConnection:(config: any) => Promise<boolean>;
     };
 }
