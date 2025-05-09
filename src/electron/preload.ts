fbd1d3c997826dec320afa9d13de105d56895448
David Rodriguez <davrdz25@yahoo.com.mx>
2025-05-09 15:25:14 -0600

    Remove unused IPC's and add test connection

--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -1,14 +1,8 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electron', {
-  setDatabaseConfig: (config: any) => ipcRenderer.invoke('set-database-config', config),
   connectDatabase: () => ipcRenderer.invoke('connect-database'),
-  existsConfigFile: () => {
-    console.log("called existsConfigFile from renderer");
-    return ipcRenderer.invoke('exists-config-file')
-  },
-  createConfigFile: (config: any) => {
-    console.log("called createConfigFile from renderer");
-    return ipcRenderer.invoke('create-config-file', config);
-}
+  existsConfigFile: () => ipcRenderer.invoke('exists-config-file'),
+  createConfigFile: (config: any) => ipcRenderer.invoke('create-config-file', config),
+  testConnection: (config: any) => ipcRenderer.invoke('test-connection',config),
 });
