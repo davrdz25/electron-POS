fb63491be613a277502d57192c15fc928bccd8fd
David Rodriguez <davrdz25@yahoo.com.mx>
2025-06-27 15:39:06 -0600

    No me juzgues solo observa xd

--- src/react/Components/Badge.tsx
+++ src/react/Components/Badge.tsx
@@ -2,12 +2,6 @@ import Style from '../Styles/Badge.module.css'
 import { TBadgeProps } from '../Types'
 import RoundedButton from './RoundedButton'
 
-{/* <div className={Style.badge} onClick={onPress}>
-                            <RoundedButton bgColor={bgColor} onPress={() => onPress} icon={String(icon)} />
-
-                <div className={Style.text} style={{ color: textColor }}>{text}</div>
-            </div> */}
-
 const Badge = ({ bgColor, icon, text, textColor = '#000000', pressable = false, onPress }: TBadgeProps) => {
     return (
         pressable ?
